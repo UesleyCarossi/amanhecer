e82c9158d943ac897a1626160bce7f2d64161334
uesley.carossi1 <uesley.carossi@evoluum.com.br>
2022-09-01 21:15:29 -0300

    first commit

---
+++ modinfo.lua
@@ -0,0 +1,25 @@
+-- This information tells other players more about the mod
+name = "Amanhaceu"
+description = "TODO"
+author = "Uesley J. Carossi"
+version = "1.00"
+
+forumthread = ""
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10--TODO
+
+--This let's the game know that this mod doesn't need to be listed in the server's mod listing
+client_only_mod = true
+
+--Let the mod system know that this mod is functional with Don't Starve Together
+dst_compatible = true
+
+all_clients_require_mod = false 
+
+-- Can specify a custom icon for this mod!
+icon_atlas = "amanhecer.xml"
+icon = "amanhecer.tex"
+
+-- Specify the priority
+priority=3--TODO
---
+++ modmain.lua
@@ -0,0 +1,8 @@
+
+local assets =
+{
+	Asset("SOUNDPACKAGE", "resources/sounds/amanheceu.fev"),
+    Asset("SOUND", "resources/sounds/amanheceu.fsb")
+}
+
+RemapSoundEvent("dontstarve/music/music_dawn_stinger", "amanheceu/amanheceu/amanheceu")
